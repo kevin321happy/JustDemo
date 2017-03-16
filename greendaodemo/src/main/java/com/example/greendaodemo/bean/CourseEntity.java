db2b1f71308d10b40d4b0b591db3265065e2c335
qiuma <527892069@qq.com>
2017-03-16 23:55:36 +0800

    11

---
+++ greendaodemo/src/main/java/com/example/greendaodemo/bean/CourseEntity.java
@@ -0,0 +1,11 @@
+package com.example.greendaodemo.bean;
+
+/**
+ * Created by kevin on 2017/3/16. 邮箱：kevin321vip@126.com 公司：锦绣氘(武汉科技有限公司)
+ * ╭╩═╮╔════╗╔════╗╔════╗╔════╗╔════╗ ╭╯ GO ╠╣   永  ╠╣  无   ╠╣   B   ╠╣   U   ╠╣  G  ╠╣
+ * ╰⊙═⊙╯╚◎══◎╝╚◎══◎╝╚◎══◎╝╚◎══◎╝╚◎══◎╝
+ */
+
+public class CourseEntity {
+
+}

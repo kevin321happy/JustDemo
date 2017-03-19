f4f855d070943b582a8cdcd06148cad0e1483097
qiuma <527892069@qq.com>
2017-03-19 11:47:55 +0800

    提交测试的Gitclass

---
+++ permission60/src/main/java/com/kevin/myapplication/GitTest.java
@@ -0,0 +1,11 @@
+package com.kevin.myapplication;
+
+/**
+ * Created by kevin on 2017/3/19. 邮箱：kevin321vip@126.com 公司：锦绣氘(武汉科技有限公司)
+ * ╭╩═╮╔════╗╔════╗╔════╗╔════╗╔════╗ ╭╯ GO ╠╣   永  ╠╣  无   ╠╣   B   ╠╣   U   ╠╣  G  ╠╣
+ * ╰⊙═⊙╯╚◎══◎╝╚◎══◎╝╚◎══◎╝╚◎══◎╝╚◎══◎╝
+ */
+
+public class GitTest {
+
+}

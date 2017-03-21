72938a873ac440013a9931070eb166146c24fc69
qiuma <527892069@qq.com>
2017-03-21 13:58:32 +0800

    ke zhan kai de listview

---
+++ exlistviewdemo/src/main/java/com/kevin/exlistviewdemo/ExListViewActivity.java
@@ -0,0 +1,18 @@
+package com.kevin.exlistviewdemo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by kevin on 2017/3/21. 邮箱：kevin321vip@126.com 公司：锦绣氘(武汉科技有限公司)
+ * 可展开的列表界面
+ */
+
+public class ExListViewActivity extends Activity {
+
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+     setContentView(R.layout.exlist_activity);
+  }
+}

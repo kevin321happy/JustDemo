5c96fcc8350cb813f4fb2cc132254ae27f41c597
qiuma <527892069@qq.com>
2017-04-14 09:10:18 +0800

    提价微信拍摄2.0

--- wechatrecord2.0/src/main/java/com/kevin/wechatrecord20/App.java
+++ wechatrecord2.0/src/main/java/com/kevin/wechatrecord20/App.java
@@ -1,6 +1,31 @@
 package com.kevin.wechatrecord20;
 
 
-public class App {
+import android.app.Application;
+import android.os.Environment;
+import java.io.File;
+import mabeijianxi.camera.VCamera;
+import mabeijianxi.camera.util.DeviceUtils;
 
+public class App extends Application {
+
+  @Override
+  public void onCreate() {
+    //设置拍摄视屏的缓存路径
+    File recordFeil = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM);
+    //检测是否是中心的手机
+    if (DeviceUtils.isZte()) {
+      if (recordFeil.exists()) {
+        VCamera.setVideoCachePath(recordFeil + "/weixin/");
+      } else {
+        VCamera.setVideoCachePath(recordFeil.getPath().replace("/sdcard/",
+            "/sdcard-ext/")
+            + "/weixin/");
+      }
+    } else {
+      VCamera.setVideoCachePath(recordFeil + "/weixin/");
+    }
+    VCamera.setDebugMode(true);
+    VCamera.initialize(this);
+  }
 }
